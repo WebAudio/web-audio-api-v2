3b641847edb4986832a503a1072023d350ed87b3
Raymond Toy <rtoy@chromium.org>
2020-08-04 12:32:06 -0700

    Copy compile.sh from v1, unchanged

---
+++ compile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+usage () {
+    echo "compile.sh [-Kh?]"
+    echo "   -K       Keep the actual-errs.txt.  Useful for updating"
+    echo "            expected-errs.txt.  (Otherwise removed when done.)"
+    echo "   -h       This help"
+    echo "   -?       This help"
+    exit 0
+}
+
+KEEP=no
+while getopts "Kh?" arg
+do
+  case $arg in
+      K) KEEP=yes ;;
+      h) usage ;;
+      \?) usage ;;
+  esac
+done
+  
+# So we can see what we're doing
+set -x
+
+# Output from bikeshed is logged here, along with a version with line
+# numbers stripped out.
+BSLOG="bs.log"
+ERRLOG="actual-errs.txt"
+
+# Remove ERRLOG when we're done with this script, but keep BSLOG so we
+# can update the expected errors.
+if [ "$KEEP" = "no" ]; then
+trap "rm $ERRLOG" 0
+fi
+
+# Run bikeshed and save the output.  You can use this output as is
+# to update expected-errs.txt.
+bikeshed --print=plain -f spec 2>&1 | tee $BSLOG
+# Remove the line numbers from the log, and make sure it ends with a
+# newline.
+sed 's;^LINE [0-9]*:;LINE:;' $BSLOG | sed -e '$a\' > $ERRLOG
+# Do the same for the expected errors and compare the two.  Any
+# differences need to be fixed.  Exit with a non-zero exit code if
+# there are any differences.
+(sed 's;^LINE [0-9]*:;LINE:;' expected-errs.txt | sed -e '$a\' | diff -u - $ERRLOG) || exit 1
+
